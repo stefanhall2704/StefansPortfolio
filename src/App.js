60ec5b51504b60df5252ea778877ee130b54cf6a
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2023-03-17 00:52:28 -0400

    Address card model

--- src/App.js
+++ src/App.js
@@ -3,6 +3,83 @@ import Sidebar from "react-sidebar";
 import { useInView } from 'react-intersection-observer';
 
 
+const Modal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  return (
+    <div className="flex justify-end mt-5-5 mr-10" >
+      <ul className="flex space-x-4 text-white text-sm">
+        <li><button onClick={openModal} className=" text-white  focus:outline-none">Contact</button></li>
+      </ul>
+    {isModalOpen && (
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+          <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
+                <i class="fa-sharp fa-regular fa-address-card"></i>
+                </div>
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">Contact Information</h3>
+                  <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                Name: Stefan Hall<br />
+                Email: stefhall.2704@gmail.com<br />
+                Number: (434)962-5099
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <button onClick={closeModal} type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-400 text-base font-medium text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+)}
+</div>
+  )
+}
+
+function HeaderBar() {
+
+
+  return (
+    <div className="bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex-shrink-0">
+            <h1 className="text-white font-bold text-xl">My Awesome App</h1>
+          </div>
+          <nav className="hidden md:block">
+            <ul className="flex space-x-4 text-white text-sm">
+              <li><a href="#" className="hover:text-gray-300">Home</a></li>
+              <li><a href="#" className="hover:text-gray-300">About</a></li>
+              
+            </ul>
+          </nav>
+        </div>
+      </div>
+</div>)
+}
+
+
+
+
+
+
 const ListItem = (props) => {
   const { link, icon, text, hideText, onClick } = props;
   return (
@@ -165,6 +242,7 @@ const Main = (props) => {
 
   return (
   <div id="main">
+    <HeaderBar />
     <Sidebar className='bg-gray-400'
     sidebar={<ExternalSidebarContent items={props.items} collapsed={collapsed} toggleCollapsed={toggleCollapsed} />}
     docked={!collapsed}
@@ -172,6 +250,7 @@ const Main = (props) => {
     onSetOpen={toggleCollapsed}
     >
       <div className="content">
+        <Modal />
         <Info />
         <Blog />
       </div>
