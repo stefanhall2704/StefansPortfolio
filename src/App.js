5b50b21e3196401ac03c6c41a6d9fb6a22319da6
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2023-03-19 23:01:50 -0400

    Self taught developer story

--- src/App.js
+++ src/App.js
@@ -149,6 +149,113 @@ const Info = () => {
   );
 };
 
+const Story = () => {
+
+  return (
+    <div
+      className={` w-10/12 h-65-rm flex flex-col justify-center ml-40 mt-56 bg-slate-300  shadow-xl`}
+      style={{ boxShadow: "-40px 40px 0px 0px rgba(0, 0, 0, 0.75)" }}
+    >
+      <div className="ml-5" >
+        <h1 className="text-6xl flex justify-center">Self-Taught Developer Story</h1>
+        <p>
+          In 2019, I was a Supervisor at a Insulated Metal Panel (IMP) plant.
+          And along with the daily tasks I had, along the way, I found different
+          tasks that should absolutely be automated. So with not knowing
+          anything other than basics in Microsoft Excel. I went to learn about
+          simple cell formulas in excel. Once gaining a good amount of knowledge
+          on that, I realized it was powerful, but only so powerful. I wound up
+          learning VBA, which is a language that is able to be used in excel to
+          write scripts that have more power to them. This is what really
+          sparked my curiosity of how technology works. <br /> <br />About a year
+          later, I found a lot of employees having to manually look for
+          supplies, going up and down hallways of shelves. When I thought of a
+          way that could fix this issue. Using Microsoft Access's Database. So I
+          learned from youtube and documentation of how to use Microsoft Access.
+          I created a multi-table database, where users throughout the plant can
+          have a user interface within Access, to insert data, change data, and
+          relocate the data for where they move supplies. This also ended up
+          helping with inventory. <br /><br /> After this I was absolutely hooked into
+          tech! I went on to take an attempt on a Coursera course,{" "}
+          <a href="https://www.coursera.org/account/accomplishments/verify/2FN2YFQEH3BM">
+            Programming Foundations with JavaScript, HTML and CSS
+          </a>
+          . I completed this course with honors. And after this. I took a little
+          bit of a break from tech. Than about 8 months later, I found an old
+          friend that was recently a Site Reliability Engineer at Microsoft and
+          was currently an Automation Integration Engineer at CFA Institute. So
+          I reached out to him about my interest in tech. And he really helped
+          me with his mentorship along the way. <br /><br />
+          <h1>Mentorship:</h1>
+          As being a mentee, I went along with numerous suggested tasks provided
+          by my mentor. This started out with earning a certificate in{" "}
+          <a href="https://www.codecademy.com/profiles/stefhall2704/certificates/6c152bd262967f8c941c9707ed636bda">
+            Python with codecademy
+          </a>
+          . This really helped me learn more of the basics in programming. For
+          loops, if statements, while loops, OOP, etc. On completion of this
+          course. I went for a simple challenge, fizzbuzz. Seemed to be fairly
+          simple. But as I quickly discovered, is that was going to be the last
+          simple challenge I would have in a long time. My next task, was to use
+          free code camp to earn a certificate in{" "}
+          <a href="https://www.freecodecamp.org/certification/stefan_hall/responsive-web-design">
+            Responsive Web Design
+          </a>
+          . This really helped me dig more into web design, which is very
+          important in full stack development. With my previous knowledge of
+          Python, I went down the route of learning{" "}
+          <a href="https://www.freecodecamp.org/certification/stefan_hall/data-analysis-with-python-v7">
+            Data Analysis with Python
+          </a>
+          . This really showed me a lot of new challenges, using numpy, pandas,
+          with Jupyter Notebooks. But really helped me learn that a programming
+          language is meant to be a tool. And certain tools work for certain
+          tasks. For myself, whenever I need to crunch data, I will definitely
+          use python with numpy and pandas. Seems to be very simple to use with
+          parsing csv's with dataframes, and even putting the data in graphs.
+          Organizing the data in such a way that would make sense for the task,
+          etc. <br /> <br /> Following the Data Analysis with Python course, is when I
+          was able to land a my first tech job as a Jr. Automation Integration
+          Engineer. With all the excitement  in the world, this really showed me
+          how humbling being in the tech industry is. At the time, each task
+          seemed impossible. So it was a lot of mentoring from my team along the
+          way. Really helped me get to the next level. During the first 4
+          months, I had a task of going through legacy code, in order to learn
+          what the tool was doing. But, unfortunately, this was written in C#.
+          Which really wound up being a very difficult problem with me. I have
+          had experience in Python, and a bit in JavaScript at this point. So
+          learning a new language, still within the first year of hard grinding
+          in tech, seemed to pose a huge set of challenges for me. But, with all
+          the odds stacked against me, I was able to overcome all the challenges
+          it gave me. It took a few months, but was well worth the time. <br /> <br />
+          After learning this tools codebase, I had a new challenge, to pair up
+          with my teammate, to rewrite the tool using a more modern framework.
+          Which obviously at this time, my main experience was with python. We
+          ended up using FastAPI as a framework, which has an out of the box
+          swagger docs. So this obviously posed its own challenges along the
+          way, digging back into the old code base, than back to new codebase,
+          to rewrite the functionality in a speedy, but secure way. After taking
+          up 10 months of time from our 2 people team on this project, this was
+          finally complete. I am recognized as the lead developer of this
+          project, where I also used pytest to test our api's, which gives us a
+          93% code coverage. <br /> <br /> By the time this was complete, I was
+          promoted to a Site Reliability Engineer. This obviously brought a huge
+          set of challenges. Learning about SLO's, SLI's, SLA's, helping
+          implement opentelemetry in each service for tracing, being able to
+          setup grafana with new data sources in order to monitor newly added
+          services, tracking upstream and downstream dependencies, etc. A big
+          part of what I have learned in this was the grafanalib library. This
+          library helped backup all of our dashboards throughout grafana. Even
+          using a specific data-source to use with grafanalib to recreate all
+          monitoring for our 4 golden signals that we can use to map all of our
+          services with there repositories, Azure Monitors App servives,
+          corresponding databases for those services, etc.
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const BlogName = () => {
   return (
     <div className="flex justify-center">
@@ -247,45 +354,57 @@ const DijkstrasDescription = () => {
     <div className="flex justify-center ml-5">
       <ul>
         <li>
-          Introduction:
-          <br />
-          As I delved into algorithmic learning, I sought out challenges to
-          build my skills, starting with small problems like 2 sum and
-          Palindrome on Code Wars. This led me to explore more advanced
-          algorithms like binary search, Branch Sums, and Depth First Search,
-          which provided a foundation for understanding pathfinding algorithms.
-          With so many options to choose from, including DFS, BFS, A* Search,
-          Greedy Best-first Search, and Dijkstra's, I decided to focus on
-          mastering Dijkstra's algorithm in-depth.
+          <p>
+            Introduction:
+            <br />
+            As I delved into algorithmic learning, I sought out challenges to
+            build my skills, starting with small problems like 2 sum and
+            Palindrome on Code Wars. This led me to explore more advanced
+            algorithms like binary search, Branch Sums, and Depth First Search,
+            which provided a foundation for understanding pathfinding
+            algorithms. With so many options to choose from, including DFS, BFS,
+            A* Search, Greedy Best-first Search, and Dijkstra's, I decided to
+            focus on mastering Dijkstra's algorithm in-depth.
+          </p>
         </li>
+        <br />
         <li>
-          Skils and Knowledge:
-          <br />
-          To put my knowledge into practice, I challenged myself to build a
-          pathfinding algorithm visualizer using JavaScript. The visualizer
-          enables users to create a canvas of grids and add starting and ending
-          nodes, which then highlights the search in real-time as the algorithm
-          progresses through each "cell" of the grid.
+          <p>
+            Skils and Knowledge:
+            <br />
+            To put my knowledge into practice, I challenged myself to build a
+            pathfinding algorithm visualizer using JavaScript. The visualizer
+            enables users to create a canvas of grids and add starting and
+            ending nodes, which then highlights the search in real-time as the
+            algorithm progresses through each "cell" of the grid.
+          </p>
         </li>
+        <br />
         <li>
-          Challenges:
-          <br />I had to overcome numerous challenges throughout the development
-          process, including timing the highlighting of squares to ensure it was
-          visible to the human eye, adding a user interface to control the
-          sequence of highlighted cells, and creating blocks that prevented the
-          algorithm from searching in certain cells. These challenges required a
-          significant amount of time and effort, but ultimately allowed me to
-          better understand how Dijkstra's algorithm could be used in a
-          real-world application.
+          <p>
+            Challenges:
+            <br />I had to overcome numerous challenges throughout the
+            development process, including timing the highlighting of squares to
+            ensure it was visible to the human eye, adding a user interface to
+            control the sequence of highlighted cells, and creating blocks that
+            prevented the algorithm from searching in certain cells. These
+            challenges required a significant amount of time and effort, but
+            ultimately allowed me to better understand how Dijkstra's algorithm
+            could be used in a real-world application.
+          </p>
         </li>
+        <br />
         <li>
-          Conclusion:
-          <br />
-          Overall, the project provided me with a deeper understanding of
-          Dijkstra's algorithm, allowed me to build my problem-solving
-          abilities, and improved my technical skills in JavaScript. I would
-          highly recommend this project to any aspiring developer seeking to
-          build their skills in algorithmic problem-solving and web development.
+          <p>
+            Conclusion:
+            <br />
+            Overall, the project provided me with a deeper understanding of
+            Dijkstra's algorithm, allowed me to build my problem-solving
+            abilities, and improved my technical skills in JavaScript. I would
+            highly recommend this project to any aspiring developer seeking to
+            build their skills in algorithmic problem-solving and web
+            development.
+          </p>
         </li>
       </ul>
     </div>
@@ -307,8 +426,8 @@ const DijkstrasAlgorithm = () => {
     const centerY = div.offsetHeight / 2;
     const deltaX = x - centerX;
     const deltaY = y - centerY;
-    const rotateX = -deltaY / 40;
-    const rotateY = deltaX / 40;
+    const rotateX = -deltaY / 30;
+    const rotateY = deltaX / 30;
     setRotateX(rotateX);
     setRotateY(rotateY);
     setGradientX((x / div.offsetWidth) * 100);
@@ -334,9 +453,12 @@ const DijkstrasAlgorithm = () => {
       onMouseLeave={handleMouseLeave}
     >
       <div>
-        <h1 className="text-6xl mt-16 flex justify-center">
+        <a
+          href="https://codepen.io/stefan_hall/full/JjBKWbL"
+          className="text-6xl mt-16 flex justify-center hover:underline"
+        >
           Dijkstra's Algorithm Visualized
-        </h1>
+        </a>
         <DijkstrasDescription />
       </div>
     </div>
@@ -424,6 +546,7 @@ const Main = (props) => {
       >
         <div className="content">
           <Info />
+          <Story />
           <Blog />
           <DijkstrasAlgorithm />
           <div className="h-54"></div>
